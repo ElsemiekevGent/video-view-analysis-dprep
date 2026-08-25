c7038164728533efcd107ddb034e7dbc1b282293
Elsemieke van Gent <elsemiekevangent@ww050856.uvt.nl>
2026-08-25 15:57:54 +0200

    Add initial analysis in script.R

---
+++ download_data.r
@@ -0,0 +1,3 @@
+data_url <- paste0("https://raw.githubusercontent.com/hannesdatta/course-dprep/refs/heads/main/material/tutorials/r-bootcamp-rev/video_view.csv")
+download.file(data_url, "video_view.csv")
+list.files()
